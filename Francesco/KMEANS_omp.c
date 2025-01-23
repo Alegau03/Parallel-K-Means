defbf0f60c90e12ef42c245de46f6ae39de12af4
Francesco Carboni <121803863+cornflexxx@users.noreply.github.com>
2025-01-23 19:50:51 +0100

    UNROLL everything ...

--- Francesco/KMEANS_omp.c
+++ Francesco/KMEANS_omp.c
@@ -25,6 +25,7 @@
 
 #define MAXLINE 2000
 #define MAXCAD 200
+#define UNROLL 2
 
 #define CALLTIME(start, call)                                                  \
   call;                                                                        \
@@ -158,15 +159,35 @@ void initCentroids(const float *data, float *centroids, int *centroidPos,
 }
 
 /*
-Function euclideanDistance: Euclidean distance
-This function could be modified
+Function euclideanDistance: distanca Euclicea tra due punti
+Modificata eliminando la radice quadrata e usando divisione in blocchi per
+diminuire i chache miss La radice quadrata non è necessaria per confrontare le
+distanze relative tra punti e centroidi, poiché il confronto è valido anche con
+le distanze al quadrato. Evitare il calcolo della radice quadrata migliora
+l’efficienza computazionale, dato che è un'operazione costosa.
 */
+// Funzione ottimizzata per il calcolo della distanza euclidea
 float euclideanDistance(float *point, float *center, int samples) {
-  float dist = 0.0;
-  for (int i = 0; i < samples; i++) {
-    dist += (point[i] - center[i]) * (point[i] - center[i]);
+  float dist = 0.0f;
+  int blockSize = 32; // Dimensione del blocco ottimale per la cache L1
+  int i, j;
+  float diff;
+
+  // Calcolo a blocchi
+  // Cliclo esterno per avanzare nei blocchi
+
+  for (i = 0; i < samples; i += blockSize) {
+    float blockDist = 0.0f; // Accumulatore temporaneo per il blocco
+    // Ciclo interno per calcolare la distanza tra i punti
+    for (j = i; j * UNROLL < i + blockSize && j * UNROLL < samples; j++) {
+      diff = (point[j] - center[j]) * (point[j] - center[j]);
+      diff += (point[j + 1] - center[j + 1]) * (point[j + 1] - center[j + 1]);
+      blockDist += diff;
+    }
+    dist += blockDist; // Somma il risultato del blocco
   }
-  return (dist);
+
+  return dist; // Restituisce la distanza al quadrato
 }
 
 /*
--- Francesco/fast_exec.sh
+++ Francesco/fast_exec.sh
@@ -1,14 +1,15 @@
 mpicc KMEANS_mpi.c -o kmeans_mpi 
-mpirun  -n 8   ./kmeans_mpi test_files/input100D2.inp 6 3000 10 0.00001 test_files/output100D2  > result_mpi
-#./kmeans_omp test_files/input2D.inp 6 3000 10 0.00001 test_files/output2D_omp > result_mpi
+gcc-14 -O3 -fopenmp KMEANS_omp.c -o kmeans_omp
+mpirun  -n 8   ./kmeans_mpi test_files/input2D.inp 6 3000 10 0.00001 test_files/output2D  > result_mpi
+./kmeans_omp test_files/input2D.inp 600 3000 1 0.00001 test_files/output2D_omp > result_omp
 
-gcc KMEANS.c -o kmeans
-./kmeans test_files/input100D2.inp 6 3000 1 0.00001 test_files/output100D2_seq > result_seq
+gcc -O3 KMEANS.c -o kmeans
+./kmeans test_files/input2D.inp 600 3000 1 0.00001 test_files/output2D_seq > result_seq
 cd test_files
-paste input2D.inp output2D > graph2D.txt   
+paste input2D.inp output2D_omp > graph2D.txt   
 paste input2D.inp output2D_seq > graph2D_seq.txt 
-#gnuplot -p plot_kmeans.gp 
-#gnuplot -p plot_kmeans_seq.gp 
+gnuplot -p plot_kmeans.gp 
+gnuplot -p plot_kmeans_seq.gp 
 cd ..
 output_file1="computation_performance"
 
